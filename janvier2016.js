cbe0cdad425196566637e12a66c123962617b553
Cl0ch <chloelatour0@gmail.com>
2016-01-17 16:54:27 +0100

    Update janvier2016.js

--- janvier2016.js
+++ janvier2016.js
@@ -34,3 +34,49 @@ function soin() {
 <button onclick="attaque()">Attaque</button>
 <button onclick="soin()">Soin</button>
 2.
+<script>
+
+// PV veut dire Points de Vie
+var PV = 12;
+var degats = getRandomMinMax(3,7);
+var soins = getRandomMinMax(1,4);
+
+function attaque() {
+degats = getRandomMinMax(3,7);  
+  if(PV-degats >= 0){
+     PV=PV-degats;
+     alert("Le monstre a perdu "+degats+" PV. Il lui en reste "+PV);
+  }
+  else {
+    PV=0;
+    alert("Le monstre est mort !");
+    }
+}
+
+function soin() {
+soins = getRandomMinMax(1,4);
+   if(PV <= 0){
+	    alert("Le monstre est mort. Il ne peut plus récupérer de PV ! ");
+    }
+    else if (PV+soins >= 12){
+        PV=12;
+        alert("Le monstre a ses points de vie déjà au maximum.");
+    }
+    else {
+        PV= PV+soins;
+        alert("Le monstre a gagné "+soins+" PV. Il en a maintenant "+PV);
+    }
+}
+
+function getRandomMinMax(min,max) {
+   return Math.floor(Math.random()*(max-min))+min;
+}
+alert(getRandomMinMax(3,7));
+alert(getRandomMinMax(1,4));
+
+
+
+
+</script>
+<button onclick="attaque()">Attaque</button>
+<button onclick="soin()">Soin</button>
